82acfcc6fa882c1efeb1b896c8b8f476aeb3721e
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-06-13 00:44:31 +0900

    feat : spark hello world 예제 등록 - gradle 프로젝트 세팅 - gradle 에 spark 의존성 추가 - Application class 세팅 및 get('/hello') 라우터 등록

---
+++ src/main/java/me/dakbutfly/spark_api/Application.java
@@ -0,0 +1,9 @@
+package me.dakbutfly.spark_api;
+
+import static spark.Spark.*;
+
+public class Application {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello");
+    }
+}
